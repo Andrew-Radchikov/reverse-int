b03760582d5eae07cff123be4a6ea3b84f9d244c
Andrew-Radchikov <superprobasite@yandex.ru>
2022-06-28 16:06:31 +0300

    task2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
   
+const numberString = String(n);
+
+let reverseNumber='';
+
+if(numberString[0] === "-"){
+   for(let i=numberString.length-1; i>0 ; i-=1){
+        reverseNumber += numberString[i];
+        } 
+    }
+else{
+    for(let i=numberString.length-1; i>=0 ; i-=1){
+        reverseNumber += numberString[i];
+        }   
+    }
+
+return Number(reverseNumber);
 }
